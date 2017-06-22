28419718cc2a8dd5427ec2cec5d0398bebdfe281
Stephane Aroca-Ouellette <stephanearocaouellette@gmail.com>
2017-06-21 21:42:19 -0700

    attempting using aggregation features (min, max, sum, mean, prod) into fc

--- segmented_mil/segmented_mil_model.py
+++ segmented_mil/segmented_mil_model.py
@@ -33,8 +33,11 @@ class SegmentedMIL(Model):
         instances = tf.concat(i_preds, 1)
         # Aggregation
         # self.pred = tf.reduce_mean(instances, axis=1)
-        self.prob = nn_layers.noisy_and_1d(instances, 2 if self.model.use_softmax else 1)
-        self.logits = nn_layers.fc(self.pred, 2, 2, 'predicted_y_mil', is_training=self.is_training, activation_fn=None,
+        self.features = tf.concat([tf.reduce_mean(instances, axis=1), tf.reduce_prod(instances, axis=1),
+                                 tf.reduce_max(instances, axis=1), tf.reduce_min(instances, axis=1),
+                                 tf.reduce_sum(instances, axis=1)], 0, name='mil_features')
+        # self.prob = nn_layers.noisy_and_1d(instances, 2 if self.model.use_softmax else 1)
+        self.logits = nn_layers.fc(self.features, 5, 2, 'predicted_y_mil', is_training=self.is_training, activation_fn=None,
                                    batch_norm=False, reuse=reuse)
         self.pred = tf.nn.softmax(self.logits, name='softmax_mil')
         self.model.generate_model(bn_decay=bn_decay, reuse=True)
@@ -42,8 +45,8 @@ class SegmentedMIL(Model):
 
     def generate_loss(self):
         if self.use_softmax:
-            loss_p = tf.nn.softmax_cross_entropy_with_logits(logits=self.prob, labels=self.label_pl,
-                                                             name='sigmoid_xentropy_mil')
+            # loss_p = tf.nn.softmax_cross_entropy_with_logits(logits=self.prob, labels=self.label_pl,
+            #                                                  name='sigmoid_xentropy_mil')
             loss_l = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.label_pl,
                                                              name='sigmoid_xentropy_mil')
             self.loss = tf.reduce_mean(loss_p + loss_l)
