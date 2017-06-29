e10a57f5e355491826208c754752a148bf96dc47
Stephane Aroca-Ouellette <stephanearocaouellette@gmail.com>
2017-06-29 14:12:44 -0700

    restructured cae model to get 16x16x16 features

--- cae/cae_model.py
+++ cae/cae_model.py
@@ -19,10 +19,10 @@ class CAE(Model):
         self.input_pl = tf.placeholder(tf.float32, shape=(self.input_shape))
         self.is_training = tf.placeholder(tf.bool, shape=())
         
-    def encode(self, input_pl, input_channels):
+    def encode(self, input_pl, in_channels):
         # Encoder
         # pool0 = nn_layers.max_pool2d(input_pl, (2,2), 'pool0')
-        conv1 = nn_layers.conv2d(input_pl, input_channels, 32, (3, 3), 'conv1', is_training=self.is_training)
+        conv1 = nn_layers.conv2d(input_pl, in_channels, 32, (3, 3), 'conv1', is_training=self.is_training)
         pool1 = nn_layers.max_pool2d(conv1, (2, 2), 'pool1')
         conv2 = nn_layers.conv2d(pool1, 32, 64, (3, 3), 'conv2', is_training=self.is_training)
         pool2 = nn_layers.max_pool2d(conv2, (2, 2), 'pool2')
@@ -30,17 +30,19 @@ class CAE(Model):
         pool3 = nn_layers.max_pool2d(conv3, (2, 2), 'pool3')
         conv4 = nn_layers.conv2d(pool3, 128, 256, (3, 3), 'conv4', is_training=self.is_training)
         pool4 = nn_layers.max_pool2d(conv4, (2, 2), 'pool4')
-        conv5 = nn_layers.conv2d(pool4, 256, 16, (1, 1), 'conv5', is_training=self.is_training)
+        conv5 = nn_layers.conv2d(pool4, 256, 16, (3, 3), 'conv5', is_training=self.is_training)
 
         self.features = nn_layers.max_pool2d(conv5, (2, 2), 'pool5')
 
-    def decode(self, input_channels):
+    def decode(self, in_channels):
         # Decoder
-        deconv1 = nn_layers.conv2d_transpose(self.features, 256, 128, (3, 3), 'deconv1', is_training=self.is_training)
-        deconv2 = nn_layers.conv2d_transpose(deconv1, 128, 64, (3, 3), 'deconv1', is_training=self.is_training)
-        deconv3 = nn_layers.conv2d_transpose(deconv2, 64, 32, (3, 3), 'deconv1', is_training=self.is_training)
-        self.pred = nn_layers.conv2d_transpose(deconv3, 32, input_channels, (2, 2), 'deconv1',
-                                               is_training=self.is_training)
+        deconv1 = nn_layers.conv2d_transpose(self.features, 16, 256, (3, 3), 'deconv1', is_training=self.is_training)
+        deconv2 = nn_layers.conv2d_transpose(deconv1, 256, 128, (3, 3), 'deconv2', is_training=self.is_training)
+        deconv3 = nn_layers.conv2d_transpose(deconv2, 128, 64, (3, 3), 'deconv3', is_training=self.is_training)
+        deconv4 = nn_layers.conv2d_transpose(deconv3, 64, 32, (3, 3), 'deconv4', is_training=self.is_training)
+        deconv5 = nn_layers.conv2d_transpose(deconv4, 32, in_channels, (3, 3), 'deconv5', is_training=self.is_training)
+        self.pred = deconv5
+
         if self.pred.get_shape().as_list() != self.input_shape:
             print "Predicted shape:", str(self.pred.get_shape().as_list())
             print "Input shape:", str(self.input_shape)
@@ -48,9 +50,9 @@ class CAE(Model):
 
     def generate_model(self, input_pl=None, bn_decay=None, reuse=None):
         input_pl = self.input_pl if input_pl is None else input_pl
-        input_channels = input_pl.get_shape()[-1].value
-        self.encode(input_pl, input_channels)
-        self.decode(input_channels)
+        in_channels = input_pl.get_shape()[-1].value
+        self.encode(input_pl, in_channels)
+        self.decode(in_channels)
 
         return self.pred
 
