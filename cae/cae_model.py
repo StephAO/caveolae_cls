bf4049dbd0aa50c0c3b9629904f3fc87d807f1fe
Stephane Aroca-Ouellette <stephanearocaouellette@gmail.com>
2017-06-29 14:01:14 -0700

    layer name error fix

--- cae/cae_model.py
+++ cae/cae_model.py
@@ -24,11 +24,11 @@ class CAE(Model):
         # pool0 = nn_layers.max_pool2d(input_pl, (2,2), 'pool0')
         conv1 = nn_layers.conv2d(input_pl, input_channels, 32, (3, 3), 'conv1', is_training=self.is_training)
         pool1 = nn_layers.max_pool2d(conv1, (2, 2), 'pool1')
-        conv2 = nn_layers.conv2d(pool1, 32, 64, (3, 3), 'conv3', is_training=self.is_training)
+        conv2 = nn_layers.conv2d(pool1, 32, 64, (3, 3), 'conv2', is_training=self.is_training)
         pool2 = nn_layers.max_pool2d(conv2, (2, 2), 'pool2')
-        conv3 = nn_layers.conv2d(pool2, 64, 128, (3, 3), 'conv4', is_training=self.is_training)
+        conv3 = nn_layers.conv2d(pool2, 64, 128, (3, 3), 'conv3', is_training=self.is_training)
         pool3 = nn_layers.max_pool2d(conv3, (2, 2), 'pool3')
-        conv4 = nn_layers.conv2d(pool3, 128, 256, (3, 3), 'conv5', is_training=self.is_training)
+        conv4 = nn_layers.conv2d(pool3, 128, 256, (3, 3), 'conv4', is_training=self.is_training)
         pool4 = nn_layers.max_pool2d(conv4, (2, 2), 'pool4')
         conv5 = nn_layers.conv2d(pool4, 256, 16, (1, 1), 'conv5', is_training=self.is_training)
 
@@ -41,7 +41,10 @@ class CAE(Model):
         deconv3 = nn_layers.conv2d_transpose(deconv2, 64, 32, (3, 3), 'deconv1', is_training=self.is_training)
         self.pred = nn_layers.conv2d_transpose(deconv3, 32, input_channels, (2, 2), 'deconv1',
                                                is_training=self.is_training)
-        assert(self.pred.shape)
+        if self.pred.get_shape().as_list() != self.input_shape:
+            print "Predicted shape:", str(self.pred.get_shape().as_list())
+            print "Input shape:", str(self.input_shape)
+            raise TypeError("Output shape not the same as input shape")
 
     def generate_model(self, input_pl=None, bn_decay=None, reuse=None):
         input_pl = self.input_pl if input_pl is None else input_pl
